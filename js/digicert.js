626dc7c5138d1241581bcd155a7eadff5cfb678f
davidjm macbook <djmlewis@mac.com>
2022-04-12 13:39:21 +0100

    aa

---
+++ js/digicert.js
@@ -0,0 +1,3 @@
+var __dcid = __dcid || [];
+__dcid.push({"cid":"DigiCertClickID_V4RsMfJQ","tag":"V4RsMfJQ"});
+(function(){var cid=document.createElement("script");cid.async=true;cid.src="//seal.digicert.com/seals/cascade/seal.min.js";var s = document.getElementsByTagName("script");var ls = s[(s.length - 1)];ls.parentNode.insertBefore(cid, ls.nextSibling);}());
